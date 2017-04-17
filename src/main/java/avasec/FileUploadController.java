f465353009f2eb33a4c8f5e6c509a16bde1a5a2c
tdr680 <tdr680@yahoo.de>
2017-04-17 20:14:05 +0200

    appl, ctx, mtyp, task, wfc file upload

--- src/main/java/avasec/FileUploadController.java
+++ src/main/java/avasec/FileUploadController.java
@@ -32,6 +32,9 @@ public class FileUploadController {
   @Autowired
   private AcodeRepository acodeRepository;
 
+  @Autowired
+  private ElementRepository elementRepository;
+
   private final StorageService storageService;
 
   @Autowired
@@ -84,7 +87,7 @@ public class FileUploadController {
           try {
             role.curr.acode.add(acodeRepository.findByExtid(Long.valueOf(acode_extid)));
           } catch (NumberFormatException e) {
-            System.out.println("Error: " + record.get("extid"));
+            System.out.println("role error: " + record.get("extid"));
           }
         }
         roleRepository.save(role);
@@ -120,6 +123,104 @@ public class FileUploadController {
     }
 
     return "";
+  }
+
+  @PostMapping("/file/element/appl")
+  public String handleApplUpload(@RequestParam("file") MultipartFile file) {
+    handleElementUpload(file, "Appl", (record -> {
+      try {
+        long extid = Long.valueOf(record.get("extid"));
+        String charid = record.get("charid");
+        String name = record.get("name");
+        elementRepository.save(new Appl(extid, charid, name));
+      } catch (Exception e) {
+        System.out.println("appl error: " + e.toString());
+      }
+    }));
+    return "";
+  }
+
+  @PostMapping("/file/element/ctx")
+  public String handleCtxUpload(@RequestParam("file") MultipartFile file) {
+    handleElementUpload(file, "Ctx", (record -> {
+      try {
+        long extid = Long.valueOf(record.get("extid"));
+        String charid = record.get("charid");
+        String name = record.get("name");
+        elementRepository.save(new Ctx(extid, charid, name));
+      } catch (Exception e) {
+        System.out.println("ctx error: " + e.toString());
+      }
+    }));
+    return "";
+  }
+
+  @PostMapping("/file/element/mtyp")
+  public String handleMtypUpload(@RequestParam("file") MultipartFile file) {
+    handleElementUpload(file, "Mtyp", (record -> {
+      try {
+        long extid = Long.valueOf(record.get("extid"));
+        String charid = record.get("charid");
+        String name = record.get("name");
+        elementRepository.save(new Mtyp(extid, charid, name));
+      } catch (Exception e) {
+        System.out.println("mtyp error: " + e.toString());
+      }
+    }));
+    return "";
+  }
+
+  @PostMapping("/file/element/task")
+  public String handleTaskUpload(@RequestParam("file") MultipartFile file) {
+    handleElementUpload(file, "Task", (record -> {
+      try {
+        long extid = Long.valueOf(record.get("extid"));
+        String charid = record.get("charid");
+        String name = record.get("name");
+        elementRepository.save(new Task(extid, charid, name));
+      } catch (Exception e) {
+        System.out.println("task error: " + e.toString());
+      }
+    }));
+    return "";
+  }
+
+  @PostMapping("/file/element/wfc")
+  public String handleWfcUpload(@RequestParam("file") MultipartFile file) {
+    handleElementUpload(file, "Wfc", (record -> {
+      try {
+        long extid = Long.valueOf(record.get("extid"));
+        String charid = record.get("charid");
+        String name = record.get("name");
+        elementRepository.save(new Wfc(extid, charid, name));
+      } catch (Exception e) {
+        System.out.println("wfc error: " + e.toString());
+      }
+    }));
+    return "";
+  }
+
+  public interface RecordHandler {
+    public void process(CSVRecord record);
+  }
+
+  public void handleElementUpload(MultipartFile file, String type, RecordHandler handler) {
+    storageService.store(file);
+
+    try (InputStream stream = file.getInputStream()) {
+
+      elementRepository.deleteByType(type);
+
+      BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+      Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader(). parse(reader);
+      for (CSVRecord record : records) {
+        handler.process(record);
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 
   @ExceptionHandler(StorageFileNotFoundException.class)
